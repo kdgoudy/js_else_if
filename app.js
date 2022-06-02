7ebf45284ceecdca3083a7cc66574490f72933a9
Koki <kgoudy64@gmail.com>
2022-06-02 13:13:13 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const dayOfWeek = 'Friday';
+
+if (dayOfWeek === 'Monday') {
+  console.log('Ugh I hate Mondays')
+} else if (dayOfWeek === 'Saturday') {
+  console.log('Yay I love Saturdays')
+} else if (dayOfWeek === 'Friday') {
+  console.log ('Just one more day baby')
+}
+
